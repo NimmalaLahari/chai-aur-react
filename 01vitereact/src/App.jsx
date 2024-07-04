d7cbd4fbfe54f723bbf1d8c8f7e521a4281fea8b
NimmalaLahari <lakshmilavanyalahari23@gmail.com>
2024-07-04 13:23:11 +0530

    Learnt the flow and structure of react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,14 @@
-
+import Chai from "./chai"
 function App() {
   
 
   return (
-  <h1>Chai aur react with vite</h1>
+    <div>
+      <Chai/>
+      <h1>chai aur react</h1>
+      <p>test para</p>
+      </div>
+  
   )
 }
 
